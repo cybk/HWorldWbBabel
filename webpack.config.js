0b24b2ac53c8ec6994a7929cead2367729253c64
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2018-03-11 12:55:34 -0600

    Added support for sass
    Updated to include support for webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const env = process.env.NODE_ENV || 'development';
+const isDev = env === 'development';
+const isProd = env === 'production';
+
 module.exports = {
     entry: {
         bundle: './src/index.js'
@@ -21,11 +25,12 @@ module.exports = {
             exclude: /node_modules/,
             use: 'babel-loader'
         },{
-            test: /\.css/,
+            test: /(\.css|\.scss)$/,
             exclude: /node_modules/,
             use: [
                 { loader: 'style-loader' },
-                { loader: 'css-loader' }
+                { loader: 'css-loader' },
+                { loader: 'sass-loader' }
             ]
         }]
     }
